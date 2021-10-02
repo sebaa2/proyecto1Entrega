511aa0c17b455f50aaed7e059b9db8f32602e680
sebaa2 <48490346+sebaa2@users.noreply.github.com>
2021-10-02 17:37:32 -0300

    cambiar algunas cosas pq si

---
+++ src/main/java/datos_extras.java
@@ -0,0 +1,35 @@
+public class datos_extras {
+    String chef;
+    String especial;
+    int anuo_creacion;
+
+    public datos_extras(String chef, String especial, int anuo_creacion) {
+        this.chef = chef;
+        this.especial = especial;
+        this.anuo_creacion = anuo_creacion;
+    }
+
+    public String getChef() {
+        return chef;
+    }
+
+    public void setChef(String chef) {
+        this.chef = chef;
+    }
+
+    public String getEspecial() {
+        return especial;
+    }
+
+    public void setEspecial(String especial) {
+        this.especial = especial;
+    }
+
+    public int getAnuo_creacion() {
+        return anuo_creacion;
+    }
+
+    public void setAnuo_creacion(int anuo_creacion) {
+        this.anuo_creacion = anuo_creacion;
+    }
+}
--- src/main/java/restaurant.java
+++ src/main/java/restaurant.java
@@ -2,5 +2,34 @@ public class restaurant {
     String Nombre;
     String Direccion;
     int puntuacion;
-    
+
+    public restaurant(String nombre, String direccion, int puntuacion) {
+        Nombre = nombre;
+        Direccion = direccion;
+        this.puntuacion = puntuacion;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
 }
